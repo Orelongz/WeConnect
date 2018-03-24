b2a74062dfbdc69598877bc4a569ef87ebba727d
PelumiLonge <longe.pelumi@yahoo.com>
2018-03-24 12:36:35 +0100

    feature: get user businesses [Finishes #156184926]

--- server/src/services/genericMessages.js
+++ server/src/services/genericMessages.js
@@ -18,17 +18,4 @@ const unauthorized = res => res.status(401).json({
   message: 'Unauthorized access to content'
 });
 
-// /**
-//  * checkUUID()
-//  * @param {Object} res express response object
-//  * @param {String} id string
-//  * @param {String} str string
-//  * @return {Object} message
-//  */
-// const checkUUID = ((res, id, str) => {
-//   if (!validator.isUUID(id)) {
-//     return notFound(res, str);
-//   }
-// });
-
 export { notFound, unauthorized };
