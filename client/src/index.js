b268f433a364b68161be1cc5ad7f92ec7d2116e0
PelumiLonge <longe.pelumi@yahoo.com>
2018-04-06 11:15:35 +0100

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import rootReducer from './rootReducer';
 import App from './components/App';
 import './style/index.scss';
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
   (<BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>),
   document.getElementById('app')
 );
 
-module.hot.accept();
+if (module.hot) {
+  // Enable Webpack hot module replacement for reducers
+  module.hot.accept('./rootReducer', () => {
+    const nextRootReducer = rootReducer;
+    store.replaceReducer(nextRootReducer);
+  });
+}
  
